ba37018c4b7d02cfcdc26ebe994c40f340f1d606
likejackonfire <gravitybringsmedown@gmail.com>
2019-06-13 21:33:36 -0700

    first commit

---
+++ store.js
@@ -0,0 +1,114 @@
+"use strict";
+
+const STORE = [
+    {
+        question:
+        "What is the characters name in fallout 1?",
+        answers: [
+            "The Chosen One",
+            "The Lone Wanderer",
+            "The Beef Supreme",
+            "The Vault Dweller"
+        ],
+        correctAnswer: "The Vault Dweller"
+    },
+    {
+        question:
+        "What year did the bombs drop?",
+        answers: [
+            "2017",
+            "2077",
+            "2133",
+            "2177"
+        ],
+        correctAnswer: "2077"
+    },
+    {
+        question:
+        "Which Fallout game can you drive a car?",
+        answers: [
+            "Fallout 1",
+            "Fallout 2",
+            "Fallout 3",
+            "Fallout 4"
+        ],
+        correctAnswer: "Fallout 2"
+    },
+    {
+        question:
+        "Who is the leader of the Tunnel Snakes?",
+        answers: [
+            "Judah Kreger",
+            "Vera Keyes",
+            "Butch DeLoria",
+            "Cliff Briscoe"
+        ],
+        correctAnswer: "Butch DeLoria"
+    },
+    {
+        question:
+        "What illness is Caesar suffering from in New Vegas?",
+        answers: [
+            "tuberculosis",
+            "kidney stones",
+            "brain tumor",
+            "syphilis"
+        ],
+        correctAnswer: "brain tumor"
+    },
+    {
+        question:
+        "What animal did the deathclaws evolve from?",
+        answers: [
+            "chameleons",
+            "wolves",
+            "goats",
+            "iguanas"
+        ],
+        correctAnswer: "chameleons"
+    },
+    {
+        question:
+        "Who created the Synths in Fallout 4?",
+        answers: [
+            "The Minutemen",
+            "The Enclave",
+            "The Brotherhood of Steel",
+            "The Institute"
+        ],
+        correctAnswer: "The Institute"
+    },
+    {
+        question:
+        "For what purpose were the vaults created?",
+        answers: [
+            "Utopian communities",
+            "Experimenting on people",
+            "Protection from nuclear war",
+            "BDSM dungeons"
+        ],
+        correctAnswer: "Experimenting on people"
+    },
+    {
+        question:
+        "Who is the GNR radio DJ i Fallout 3?",
+        answers: [
+            "Three Dog",
+            "Bobby Randall",
+            "Gob Bluth",
+            "Big Joe Mosquito"
+        ],
+        correctAnswer: "Three Dog"
+    },
+    {
+        question:
+        "Which famous actor did not voice a character from the Fallout games?",
+        answers: [
+            "Matthew Perry",
+            "Liam Neeson",
+            "Danny Trejo",
+            "Willem Defoe"
+        ],
+        correctAnswer: "Willem Defoe"
+    }
+];
